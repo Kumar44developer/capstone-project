8f46241c3207e1524c7e93e8647223a9b9b9e28c
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-07-14 21:56:36 +0530

    Fetched and set states and stats data

--- frontend/src/pages/DemoPortal.jsx
+++ frontend/src/pages/DemoPortal.jsx
@@ -35,3 +35,6 @@ export default function DemoPortal() {
         apiService.getStates(),
         apiService.getStats()
       ]);
+      setStates(statesData);
+      setStats(statsData);
+    } catch (err) {
