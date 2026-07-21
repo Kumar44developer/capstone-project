bc5723e2c8621e572c0a1f1a22d43ffd8d14f161
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-07-21 22:26:53 +0530

    Added admin authentication middleware

--- src/middleware/adminAuth.js
+++ src/middleware/adminAuth.js
@@ -1,3 +1,9 @@
 const { PrismaClient } = require('@prisma/client');
 
 const prisma = new PrismaClient();
+
+module.exports = async (req, res, next) => {
+  try {
+    const user = await prisma.user.findUnique({
+      where: { id: req.user.userId }
+    });
