41005a55be99048b4c1f5ac03c8f8724e77c0a3c
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-07-24 21:01:58 +0530

    Added registration endpoint to auth router

--- src/auth.js
+++ src/auth.js
@@ -6,3 +6,6 @@ const jwt = require('jsonwebtoken');
 const router = express.Router();
 const prisma = new PrismaClient();
 
+router.post('/register', async (req, res) => {
+  try {
+    const { email, name, password } = req.body;
