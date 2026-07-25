a79990dc5204e23f2baa553135a034b404fdbb8e
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-07-25 22:11:11 +0530

    Included user details in login response

--- src/auth.js
+++ src/auth.js
@@ -104,3 +104,8 @@ router.post('/login', async (req, res) => {
       message: 'Login successful',
       token,
       user: {
+        id: user.id,
+        email: user.email,
+        name: user.name,
+        tier: user.tier
+      }
