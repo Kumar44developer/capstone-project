ed8e162513617c7495465befe9f3bf31a32cacaa
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-07-31 22:12:21 +0530

    Addrf dotenv, PrismaClient, and rateLimit dependencies

--- src/server.js
+++ src/server.js
@@ -1,2 +1,5 @@
 const express = require('express');
 const cors = require('cors');
+const dotenv = require('dotenv');
+const { PrismaClient } = require('@prisma/client');
+const rateLimit = require('express-rate-limit');
